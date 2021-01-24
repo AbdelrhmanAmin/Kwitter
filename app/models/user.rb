9faeabc00211b37d1ae3146a0dddd126a9499a39
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-24 23:27:00 +0200

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :username, length: { in: 3..12 }
+  validates :fullname, presence: true, length: { maximum: 20 }
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { in: 6..16 }
   has_many :posts
   has_many :followings
   has_one_attached :image, :dependent => :destroy
