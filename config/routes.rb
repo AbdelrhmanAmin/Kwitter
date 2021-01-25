3d44838591d91b49aa09068ee1daf1d8e88b1240
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-25 18:42:38 +0200

    Sign up and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
