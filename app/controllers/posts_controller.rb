219588a5d6eb114e5bac8d2dfe065f142cb032db
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-26 00:45:08 +0200

    Homepage functionality done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
   def index
-  @posts = Post.all
+  @posts = Post.all.order(created_at: :desc)
+  @post = Post.new
+  @users = User.all
+  end
+  def new
+  end
+  def create
+    @post = current_user.posts.build(post_params)
+
+      if @post.save
+        redirect_to root_path
+      end
+  end
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
