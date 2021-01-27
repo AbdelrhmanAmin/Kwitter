b6bc27711ddcc0e9c262851ef7d5b8a305c300ce
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-28 00:30:46 +0200

    Done moving all view logic to respective helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -27,4 +27,92 @@ module EventsHelper
       'Attendees'
     end
   end
+  def error_msg(event)
+    out = ''
+    if event.errors.any?
+     out << "<div id='error_explanation'>
+        <h2>
+        #{pluralize(event.errors.count, "error")} prohibited this event from being saved:
+        </h2>
+        <ul>
+          #{err_msg(event)}
+        </ul>
+      </div>"
+    end
+    out.html_safe
+  end
+  def err_msg(event)
+    out = ''
+       event.errors.full_messages.each do |message|
+        out << "<li>#{message}</li>"
+      end
+    out.html_safe
+  end
+  def header_events
+    out = ''
+    if user_signed_in?
+      out << "
+      <li>#{link_to 'Create Event', new_event_path, class: 'header-links  text-white h5 mr-5'}</li>
+      <li>#{link_to current_user.username, user_path(current_user), class: 'header-links h5 mr-5'}</li>
+      <li>#{link_to 'logout', logout_path, class: 'header-links h5 mr-5'}</li>"
+    else
+      out << "<li>#{link_to 'Login', login_path, class: 'mx-2'}</li>
+      <li>#{link_to 'signup', signup_path, class: 'mx-2'}</li>"
+    end
+    out.html_safe
+  end
+  def links_events(current_user)
+    out = ''
+      if user_signed_in?
+        out << "<div class='d-flex w-100 px-3 py-2 align-items-center'>
+          <i class='fa fa-user'></i>
+          #{link_to 'Profile', user_path(current_user), class: 'w-100 h6 links-color ml-3'}
+        </div>
+          <div class='d-flex bg-select w-100  px-3 py-2 align-items-center'>
+            <i class='fas fa-calendar-alt text-white'></i>
+              #{link_to 'Events', events_path, class: 'w-100 h6 text-white links-color ml-3'}
+          </div>"
+      else
+        out << "<div class='d-flex  w-100  px-3 py-2 align-items-center'>
+          <i class='fas fa-calendar-alt'></i>
+            #{link_to 'Events', events_path, class: 'w-100 h6 links-color ml-3'}
+        </div>
+        <ul class='d-flex  w-100 mx-2  py-2 align-items-start flex-column list-unstyled'>
+          <li>#{link_to 'Login', login_path}</li>
+          <li>#{link_to 'signup', signup_path}</li>
+        </ul>"
+      end
+    out.html_safe
+  end
+  def upcoming_events(coming_events)
+    out = ''
+    if coming_events.length > 0
+       coming_events.each do |event|
+      out << "<tr>
+          <td>#{event.title}</td>
+          <td>#{event.creator.username}</td>
+          <td>#{event.date}</td>
+          <td>#{link_to 'Show', event, class: 'text-success'}</td>
+        #{if current_user == event.creator
+            "<td>#{editing_events(event, current_user)}</td>
+            <td>#{deleting_events(event, current_user)}</td>"
+        end}
+      </tr>"
+      end
+    end
+    out.html_safe
+  end
+  def old_events(prev_events)
+    out = ''
+    if prev_events.length > 0
+      prev_events.each do |event|
+       out << "<tr>
+          <td>#{event.title }</td>
+          <td>#{event.creator.username }</td>
+          <td>#{event.date }</td>
+        </tr>"
+      end
+    end
+    out.html_safe
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,120 @@
 module PostsHelper
+  def header(current_user)
+    out = ''
+    if user_signed_in?
+      out << "<li>#{link_to current_user.username, user_path(current_user), class: 'header-links h5 mr-5'}</li>
+      <li> #{link_to 'logout', logout_path, class: 'header-links h5 mr-5'} </li>"
+    else
+      out << "<li> #{link_to 'Login', login_path, class: 'mx-2'} </li>
+      <li> #{link_to 'signup', signup_path, class: 'mx-2'} </li>"
+    end
+    out.html_safe
+  end
+  def who_to_follow(users, current_user)
+    out = ''
+    if user_signed_in? 
+      users.each do |user| 
+        if !current_user.following?(user) && current_user != user 
+          out << "<div class='d-flex flex-column px-4 border border-right-0 border-left-0 border-top-0'>
+            <div class='d-flex py-3'>
+              #{image_tag user.image, style: "width: 50px; height: 50px; display:block; border-radius: 50%"}
+              #{link_to user.username, user_path(user), class: "h6 m-3 links-color" }
+            </div>
+          </div>"
+         end 
+      end
+    end
+    out.html_safe
+  end
+  def dashboard_posts(current_user)
+    out = ''
+    if user_signed_in?
+      out << "<div class='d-flex  align-items-center py-3 px-3'>
+          #{image_tag current_user.profile_picture(current_user)}
+          #{link_to current_user.username, user_path(current_user), class: 'h6 m-3 links-color'}
+        </div>
+        <div class='d-flex justify-content-around followers px-1'>
+          <div class='d-flex flex-column align-items-center border py-2 px-4 border-dark border-left-0'>
+            <span class='followers-num'> #{current_user.following.count} </span>
+            <span>
+              Following
+            </span>
+          </div>
+          <div class='d-flex flex-column align-items-center border border-right-0 py-2 px-4 border-dark '>
+            <span class='followers-num'> #{current_user.followers.count} </span>
+            <span>
+              Followers
+            </span>
+          </div>
+        </div>"
+    end
+    out.html_safe
+  end
+  def links_posts(current_user)
+    out = ''
+      if user_signed_in?
+        out << "<div class='d-flex w-100 px-3 py-2 align-items-center'>
+          <i class='fa fa-user'></i>
+          #{link_to 'Profile', user_path(current_user), class: 'w-100 h6 links-color ml-3'}
+        </div>
+          <div class='d-flex  w-100  px-3 py-2 align-items-center'>
+            <i class='fas fa-calendar-alt'></i>
+              #{link_to 'Events', events_path, class: 'w-100 h6 links-color ml-3'}
+          </div>"
+      else
+        out << "<div class='d-flex  w-100  px-3 py-2 align-items-center'>
+          <i class='fas fa-calendar-alt'></i>
+            #{link_to 'Events', events_path, class: 'w-100 h6 links-color ml-3'}
+        </div>
+        <ul class='d-flex  w-100 mx-2  py-2 align-items-start flex-column list-unstyled'>
+          <li>#{link_to 'Login', login_path}</li>
+          <li>#{link_to 'signup', signup_path}</li>
+        </ul>"
+      end
+    out.html_safe
+  end
+  def tweet_posts(post)
+    out = ''
+    if user_signed_in?
+      out << "<div class='bg-white p-3 m-5'>
+        <h4>TWEET</h4>
+        #{form_for post, html: { class: 'form-inline' } do |form| 
+          concat form.text_field :content, class: 'form-control', placeholder: 'compose a tweet' 
+          concat form.submit 'TWEET', class: 'btn btn-info text-white'
+        end}
+      </div>"
+    end
+    out.html_safe
+  end
+  def posts(posts)
+    out = ''
+      if posts.length > 0
+        posts.each do |post|
+           out << " <div class='d-flex py-3'>
+              #{image_tag post.user.image, style: "width: 50px; height: 50px; display:block;"}
+              <div class='d-flex flex-column  ml-3'>
+                #{link_to post.user.username, user_path(post.user), class: "h6 links-color"}
+                <span class='text-secondary'>#{post.content}</span>
+              </div>
+            </div>"
+        end
+      else
+        out <<"<h5 class='p-3'>Be the first one to tweet.</h5>"
+      end
+      out.html_safe
+  end
+  def events(coming_events)
+    out = ''
+    coming_events.each do |event|
+    out << "
+      <div class='d-flex py-3'>
+        #{image_tag event.creator.image, style: "width: 50px; height: 50px; display:block;"}
+        <div class='d-flex flex-column  ml-3'>
+          #{link_to event.creator.username, user_path(event.creator), class: "h6 links-color"}
+          <span class='text-secondary'>I've just hosted <strong>#{event.title}</strong> come and #{link_to 'attend !', event_path(event)}</span>
+        </div>
+      </div>"
+    end
+    out.html_safe
+  end
 end
