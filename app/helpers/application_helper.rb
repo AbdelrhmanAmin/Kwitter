085e79b6a8f3214eb02d5640ca3105a981e5616d
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-29 02:50:14 +0200

    fix image displaying for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def img_replacer(user)
    out=''
    out << "#{ if user.image.attached?
-      image_tag user.profile_picture(user)
+      image_tag user.profile_picture(user), style: "width: 50px; height:50px"
     else
       image_tag('https://placekitten.com/200/139', style: "width: 50px; height:50px")
     end
@@ -12,9 +12,9 @@ module ApplicationHelper
   def cover_replacer(user)
     out=''
     out << "#{ if user.image.attached?
-      image_tag user.profile_picture(user)
+      image_tag user.profile_picture(user), :style => "width: 100%; height:250px"
       else
-      image_tag('https://placekitten.com/850/300', style: "width: 850px; height:250px")
+      image_tag('https://placekitten.com/850/300', :style => "width: 100%; height:250px")
       end
       }"
     out.html_safe
@@ -22,7 +22,7 @@ module ApplicationHelper
   def event_replacer(event)
     out=''
     out << "#{ if event.cover.attached?
-      image_tag event.profile_picture(event)
+      image_tag event.creator.profile_picture(event.creator), style: "width: 100%; height:250px;"
       else
       image_tag('https://placekitten.com/250/300', style: "width: 100%; height:250px;")
       end
