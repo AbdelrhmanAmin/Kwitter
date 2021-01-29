a61d44f854aaa27a7ac6af4c76c5935ace706777
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-29 03:04:31 +0200

    Fix heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,8 @@ module ApplicationHelper
   end
   def cover_replacer(user)
     out=''
-    out << "#{ if user.image.attached?
-      image_tag user.profile_picture(user), :style => "width: 100%; height:250px"
+    out << "#{ if user.cover.attached?
+      image_tag user.thumbnail(user), :style => "width: 100%; height:250px"
       else
       image_tag('https://placekitten.com/850/300', :style => "width: 100%; height:250px")
       end
@@ -29,5 +29,14 @@ module ApplicationHelper
       }"
     out.html_safe
   end
-  
+  def profile_replacer(user)
+   out=''
+   out << "#{ if user.image.attached?
+      image_tag user.profile_picture(user), style: "width: 90px; height:90px"
+    else
+      image_tag('https://placekitten.com/200/139', style: "width: 90px; height:90px")
+    end
+    }"
+    out.html_safe
+  end
 end
